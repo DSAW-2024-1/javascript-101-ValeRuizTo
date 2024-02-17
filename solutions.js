87cf8cd95546ac61f05e6565f0138288263aee97
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-02-17 13:30:24 -0500

    commit1

--- solutions.js
+++ solutions.js
@@ -40,7 +40,7 @@ function findLargest(arr) {
   }
   else{
     let numeroMayor=arr[0]
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
       if (arr[i] > numeroMayor) {
           numeroMayor = arr[i]; 
   }
